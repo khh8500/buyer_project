4a136c6b6cd5138b7ff72a326aff67b2869e32b7
khh <khh8500@naver.com>
2024-05-04 16:50:47 +0900

    환경설정

---
+++ src/main/java/com/example/buyer_project/BuyerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.buyer_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyerProjectApplication.class, args);
+    }
+
+}
